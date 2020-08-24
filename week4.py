91378643f79d447c1087ff9509b5a20e75de3370
John Berrisford <berrisfordjohn@gmail.com>
2020-08-24 08:14:23 +0100

    cleaned up in pycharm

--- week4.py
+++ week4.py
@@ -1,9 +1,9 @@
 import os
-import pandas as pd # library for data analysis
-from bs4 import BeautifulSoup # library to parse web pages
-import requests # library to handle requests
+import pandas as pd  # library for data analysis
+from bs4 import BeautifulSoup  # library to parse web pages
+import requests  # library to handle requests
 import csv
-import folium # map rendering library
+import folium  # map rendering library
 from sklearn.cluster import KMeans
 import numpy as np
 # Matplotlib and associated plotting modules
@@ -29,6 +29,8 @@ class ProcessLocation:
         self.longitude = []
         self.latitude = []
         self.data_from_wikipedia = []
+        self.df = None
+        self.grouped_df = None
         self.nearby_venues = None
         self.neighborhoods_venues_sorted = None
         self.read_local_coordinates_file()
@@ -38,7 +40,7 @@ class ProcessLocation:
             data = csv.DictReader(in_file)
             for row in data:
                 self.coordinate_data[row['Postal Code']] = {'longitude': row['Longitude'],
-                                                    'latitude': row['Latitude']}
+                                                            'latitude': row['Latitude']}
 
     def get_coordinates(self, postal_code):
         ret = self.coordinate_data.get(postal_code, {})
@@ -51,10 +53,10 @@ class ProcessLocation:
         if self.url:
             req = requests.get(self.url)
             soup = BeautifulSoup(req.content, 'html.parser')
-            #print(soup.prettify())
-            table = soup.find('table', attrs={'class':'wikitable sortable'})
+            # print(soup.prettify())
+            table = soup.find('table', attrs={'class': 'wikitable sortable'})
             table_body = table.find('tbody')
-            #print(table_body)
+            # print(table_body)
 
             # get the headers of the table and store in a list
             table_headers = []
@@ -80,7 +82,6 @@ class ProcessLocation:
                         row_data['Neighborhood'] = row_data.pop('Neighbourhood')
                     self.data_from_wikipedia.append(row_data)
 
-
     def load_data_into_dataframe(self):
         if self.data_from_wikipedia:
             self.df = pd.DataFrame(self.data_from_wikipedia)
@@ -100,12 +101,11 @@ class ProcessLocation:
         self.df['Latitude'] = self.latitude
         self.df['Longitude'] = self.longitude
 
-        
-    def getNearbyVenues(self, radius=500):
+    def get_nearby_venues(self, radius=500):
         names = self.df['Neighborhood']
         latitudes = self.df['Latitude']
         longitudes = self.df['Longitude']
-        venues_list=[]
+        venues_list = []
         for name, lat, lng in zip(names, latitudes, longitudes):
             url = 'https://api.foursquare.com/v2/venues/explore?&client_id={}&client_secret={}&v={}&ll={},{}&radius={}&limit={}'.format(
                 CLIENT_ID,
@@ -127,12 +127,12 @@ class ProcessLocation:
 
         self.nearby_venues = pd.DataFrame([item for venue_list in venues_list for item in venue_list])
         self.nearby_venues.columns = ['Neighborhood',
-                    'Neighborhood Latitude',
-                    'Neighborhood Longitude',
-                    'Venue',
-                    'Venue Latitude',
-                    'Venue Longitude',
-                    'Venue Category']
+                                      'Neighborhood Latitude',
+                                      'Neighborhood Longitude',
+                                      'Venue',
+                                      'Venue Latitude',
+                                      'Venue Longitude',
+                                      'Venue Category']
 
     def classification_of_venues(self):
         print('There are {} uniques categories.'.format(len(self.nearby_venues['Venue Category'].unique())))
@@ -146,7 +146,7 @@ class ProcessLocation:
 
         self.grouped_df = onehot.groupby('Neighborhood').mean().reset_index()
         print(self.grouped_df.head())
-        
+
     def sort_top_ten_venues(self):
         print('sort top ten venues')
         # new dataframe and display the top 10 venues for each neighborhood.
@@ -157,10 +157,10 @@ class ProcessLocation:
         # create columns according to number of top venues
         columns = ['Neighborhood']
         for ind in np.arange(num_top_venues):
-            columns.append('{}'.format(ind +1))
-            #try:
+            columns.append('{}'.format(ind + 1))
+            # try:
             #    columns.append('{}{} Most Common Venue'.format(ind+1, indicators[ind]))
-            #except:
+            # except:
             #    columns.append('{}th Most Common Venue'.format(ind+1))
 
         # create a new dataframe
@@ -168,16 +168,16 @@ class ProcessLocation:
         self.neighborhoods_venues_sorted['Neighborhood'] = self.grouped_df['Neighborhood']
 
         for ind in np.arange(self.grouped_df.shape[0]):
-            self.neighborhoods_venues_sorted.iloc[ind, 1:] = return_most_common_venues(self.grouped_df.iloc[ind, :], num_top_venues)
+            self.neighborhoods_venues_sorted.iloc[ind, 1:] = return_most_common_venues(self.grouped_df.iloc[ind, :],
+                                                                                       num_top_venues)
 
         print(self.neighborhoods_venues_sorted)
-
 
     def cluster_data(self):
         # set number of clusters
         kclusters = 5
 
-        grouped_clustering = grouped.drop('Neighborhood', 1)
+        grouped_clustering = self.grouped_df.drop('Neighborhood', 1)
 
         # run k-means clustering
         kmeans = KMeans(n_clusters=kclusters, random_state=0).fit(grouped_clustering)
@@ -186,18 +186,19 @@ class ProcessLocation:
         kmeans.labels_[0:10]
 
         # add clustering labels
-        neighborhoods_venues_sorted.insert(0, 'Cluster Labels', kmeans.labels_)
+        self.neighborhoods_venues_sorted.insert(0, 'Cluster Labels', kmeans.labels_)
 
-        toronto_merged = df
+        toronto_merged = self.df
 
         # merge toronto_grouped with toronto_data to add latitude/longitude for each neighborhood
-        toronto_merged = toronto_merged.join(neighborhoods_venues_sorted.set_index('Neighborhood'), on='Neighborhood')
+        toronto_merged = toronto_merged.join(self.neighborhoods_venues_sorted.set_index('Neighborhood'),
+                                             on='Neighborhood')
 
         toronto_merged.head()
 
         # set color scheme for the clusters
         x = np.arange(kclusters)
-        ys = [i + x + (i*x)**2 for i in range(kclusters)]
+        ys = [i + x + (i * x) ** 2 for i in range(kclusters)]
         colors_array = cm.rainbow(np.linspace(0, 1, len(ys)))
         rainbow = [colors.rgb2hex(i) for i in colors_array]
 
@@ -216,7 +217,7 @@ class ProcessLocation:
         if os.path.exists(dataframe_pickle):
             self.nearby_venues = pd.read_pickle(dataframe_pickle)
         else:
-            self.getNearbyVenues()
+            self.get_nearby_venues()
             self.nearby_venues.to_pickle(dataframe_pickle)
 
     def process_url(self):
@@ -232,9 +233,11 @@ class ProcessLocation:
         location_url = {'toronto': 'https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M'}
         self.url = location_url.get(self.location.lower())
 
+
 def main():
     pl = ProcessLocation('toronto')
     pl.run_process()
 
+
 if __name__ == '__main__':
-    main()    
+    main()
